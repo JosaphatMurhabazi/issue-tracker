2e4ca7cfbf9ea240d98bc41ad9b3df435c3663eb
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-08-31 19:30:11 +0100

    Add a Markdown Editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,19 @@
+'use client';
 import React from 'react'
-import {Button, TextArea, TextField} from "@radix-ui/themes";
+import {Button, TextField} from "@radix-ui/themes";
+import dynamic from 'next/dynamic'
+import "easymde/dist/easymde.min.css";
+
+const SimpleMDE = dynamic(
+    () => import("react-simplemde-editor"),
+    { ssr: false }
+);
 
 const NewIssuePage = () => {
     return (
         <div className='max-w-xl space-y-3'>
             <TextField.Root placeholder='Title'></TextField.Root>
-            <TextArea placeholder='Description'/>
+            <SimpleMDE placeholder='Description'/>
             <Button>Submit New Issue</Button>
         </div>
     )
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Theme } from "@radix-ui/themes";
+import {Theme} from "@radix-ui/themes";
 import Navbar from "@/app/Navbar";
 
 const geistSans = Geist({
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <Theme>
+      <Theme accentColor="violet" appearance="light">
           <Navbar />
           <main className='p-5'>{children}</main>
       </Theme>
