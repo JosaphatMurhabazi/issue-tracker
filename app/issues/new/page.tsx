d2b200bb38c1d1d331cc90e165bf47652d52aabc
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-08-31 20:56:00 +0100

    Add a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, {useState} from 'react'
 import axios from "axios";
-import {Button, Callout, TextField,Text} from "@radix-ui/themes"
+import {Button, Callout, TextField} from "@radix-ui/themes"
 import dynamic from 'next/dynamic'
 import "easymde/dist/easymde.min.css"
 import {useForm,Controller} from "react-hook-form";
@@ -10,6 +10,7 @@ import {zodResolver} from "@hookform/resolvers/zod";
 import {issueSchema} from "@/app/validationSchema";
 import {z} from 'zod'
 import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
 
 const SimpleMDE = dynamic(
@@ -25,6 +26,7 @@ const NewIssuePage = () => {
         resolver:zodResolver(issueSchema)
     })
     const [error, setError] = useState<string>('');
+    const [isSubmitting, setSubmitting] = useState<boolean>(false);
 
     return (
         <div className='max-w-xl'>
@@ -34,9 +36,11 @@ const NewIssuePage = () => {
                 </Callout.Root>)}
         <form className='space-y-3' onSubmit={handleSubmit(async(data)=>{
             try{
+                setSubmitting(true);
                 await axios.post("/api/issues",data);
                 router.push("/issues")
             }catch (error){
+                setSubmitting(false);
                 setError('An unexpected error occurred.');
             }
 
@@ -47,7 +51,7 @@ const NewIssuePage = () => {
             {<ErrorMessage>{errors.description?.message}</ErrorMessage>}
 
 
-            <Button>Submit New Issue</Button>
+            <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner/>}</Button>
         </form></div>
     )
 }
