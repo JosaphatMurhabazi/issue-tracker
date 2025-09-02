ce8121cb9accea83766ca9029782fb80f646d0fd
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-09-02 11:12:47 +0100

    Add delete button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Inter} from "next/font/google";
 import "./globals.css";
-import {Theme} from "@radix-ui/themes";
+import {Container, Theme} from "@radix-ui/themes";
 import Navbar from "@/app/Navbar";
 import {ReactNode} from "react";
 
@@ -27,7 +27,9 @@ export default function RootLayout({
         className={inter.variable}>
       <Theme accentColor="violet" appearance="light">
           <Navbar />
-          <main className='p-5'>{children}</main>
+          <main className='p-5'>
+              <Container>{children}</Container>
+          </main>
       </Theme>
 
 
