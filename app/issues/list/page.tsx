01a5ae7742f709805f9a00a2b4a7db6809198f89
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-09-10 21:21:55 +0100

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -20,7 +20,9 @@ const IssuesPage = async ({searchParams}: Props) => {
     const statuses =Object.values(Status);
     const status = statuses.includes(searchParams.status)?searchParams.status : undefined;
 
-        const issues = await prisma.issue.findMany({where: {status}});
+    const orderBy =columns.map(column =>column.value).includes(searchParams.orderBy) ? {[searchParams.orderBy]: 'asc'}:undefined;
+
+        const issues = await prisma.issue.findMany({where: {status},orderBy});
 
     return (
         <div>
