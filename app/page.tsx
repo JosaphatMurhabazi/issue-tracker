418b52b4325f59abafd622bc61bf37299995f1fd
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-09-13 17:52:32 +0100

    Build the IssueChart component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import IssueSummary from "@/app/IssueSummary";
 import {prisma} from "@/prisma/client";
+import IssueChart from "@/app/IssueChart";
 
 export default async function Home() {
     const open =await prisma.issue.count({where:{status:'OPEN'}});
@@ -7,6 +7,6 @@ export default async function Home() {
     const closed =await prisma.issue.count({where:{status:'CLOSED'}});
 
     return (
-        <IssueSummary open={open} inProgress={inProgress} closed={closed}/>
+        <IssueChart open={open} inProgress={inProgress} closed={closed}/>
     );
 }
